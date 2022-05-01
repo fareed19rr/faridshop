6d4a579dd425f9267e20bf81ed4079b7e35bdc22
Muhamad Farid Ridho Rambe <faridridho@192.168.1.14>
2022-05-01 13:59:41 +0700

    Fetching API into client side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
 import Header from './components/header'
 import Footer from './components/Footer';
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({})
+
   const { id } = useParams()
-  const product = products.find((p) => p._id === (id))
-  console.log(product)
-  
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [id])
+
   return (
     <>
-        <Link className='btn btn-dark' my-3 to='/'>
-          <i class="fas fa-arrow-left"></i>
+        <Link className='btn btn-dark' to='/'>
+          <i className="fas fa-arrow-left"></i>
         </Link>
         
         <Row>
